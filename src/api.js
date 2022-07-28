7072531db87ecbd97079a08a82c9d143dc258652
James Anuncio <99728604+yada-kun@users.noreply.github.com>
2022-07-28 21:41:49 +0800

    updated api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://cloudsky-webnovel-be.herokuapp.com/';
 
 export const getNovels = () => {
     return axios.get(`${BASE_URL}/api/novels`)
